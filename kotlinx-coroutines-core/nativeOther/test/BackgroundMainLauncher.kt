95363cc51c8a620210d83afd53de9bb33a37526f
Roman Elizarov <elizarov@gmail.com>
2019-11-09 18:02:52 +0300

    ~ Background test runs on Windows and Linux

--- kotlinx-coroutines-core/nativeOther/test/BackgroundMainLauncher.kt
+++ kotlinx-coroutines-core/nativeOther/test/BackgroundMainLauncher.kt
@@ -4,19 +4,15 @@
 
 package kotlinx.coroutines
 
-import platform.CoreFoundation.*
 import kotlin.native.concurrent.*
 import kotlin.native.internal.test.*
 import kotlin.system.*
 
 // This is a separate entry point for tests
 fun mainBackground(args: Array<String>) {
-    initMainThread() // not really needed, since runtime is initialized by the main thread here anyway
     val worker = Worker.start(name = "main-background")
     worker.execute(TransferMode.SAFE, { args.freeze() }) {
         val result = testLauncherEntryPoint(it)
         exitProcess(result)
-    }
-    CFRunLoopRun()
-    error("CFRunLoopRun should never return")
+    }.result // block main thread
 }
